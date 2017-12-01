3d53c3bf96d13207099d85d919fc664ae5eb372d
TickleMeeHomo <32668823+TickleMeeHomo@users.noreply.github.com>
2017-12-01 10:13:28 +0100

    hvad sker der

--- MobilReklameApp/DomainClasses/Order.cs
+++ MobilReklameApp/DomainClasses/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,40 @@ namespace MobilReklameApp.DomainClasses
 {
     class Order
     {
+        private string _ordreBeskrivelse;
+        private int _ordreDato;
+        private string _ordreLeveringsDato;
+        private int _ordreNummer;
+        private string _produktType;
+        private string _ordreStatus;
 
 
+        public Order()
+        {
+            _ordreBeskrivelse = OrdreBeskrivelse;
+            _ordreDato = OrdreDato;
+            _ordreLeveringsDato = OrdreLeveringsDato;
+            _ordreNummer = OrdreNummer;
+            _produktType = ProduktType;
+            _ordreStatus = OrdreStatus;
 
+        }
 
-
-
-
-
-
-
-
+    public string OrdreBeskrivelse
+    {
+        get { return _ordreBeskrivelse; }
+        
     }
-}
+
+    public string OrdreBeskrivelse
+        {
+            get { return _ordreBeskrivelse; }
+
+        }
+
+    public string OrdreBeskrivelse
+        {
+            get { return _ordreBeskrivelse; }
+
+        }
+    }
