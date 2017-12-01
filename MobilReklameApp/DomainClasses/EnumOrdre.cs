b29e764933cc0a26a5eacdfce119c12f5ff088f0
TickleMeeHomo <32668823+TickleMeeHomo@users.noreply.github.com>
2017-12-01 11:41:08 +0100

    OrdreEnum

---
+++ MobilReklameApp/DomainClasses/EnumOrdre.cs
@@ -0,0 +1,18 @@
+﻿namespace MobilReklameApp.DomainClasses
+{
+    public class EnumOrdre
+    {
+        enum OrdreStatus
+        {
+            Afventer,
+            Accepteret,
+            Anulleret,
+            Igangværende,
+            Færdig,
+            Leveret
+        }
+
+
+    }
+    
+}
