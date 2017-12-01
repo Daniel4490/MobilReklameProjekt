1a17cf663c20f3e391ea2c882f7aeb3ba65e73fc
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2017-12-01 09:57:56 +0100

    CustomerCatalog

---
+++ MobilReklameApp/DomainClasses/CustomerCatalog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobilReklameApp.BaseClasses;
+
+namespace MobilReklameApp.DomainClasses
+{
+    class CustomerCatalog : CatalogBase<Customer>
+    {
+        private Dictionary<int, Customer> _customers;
+
+        public CustomerCatalog()
+        {
+            
+        }
+        public List<Customer> listAll
+        {
+            get { return _customers.Values.ToList(); }
+        }
+
+        public void Add(int tlf, Customer customer)
+        {
+            _customers.Add(tlf, customer);
+        }
+
+
+
+    }
+}
