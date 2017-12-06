c4d045c20e8502071e90a13118154faebb8b57ea
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2017-12-06 11:23:38 +0100

    Used DRY on PublicCustomer

--- MobilReklameApp/SubClasses/PublicCustomer.cs
+++ MobilReklameApp/SubClasses/PublicCustomer.cs
@@ -10,49 +10,14 @@ namespace MobilReklameApp.SubClasses
 {
     class PublicCustomer : PeopleBase
     {
-        private string _firstName;
-        private string _lastName;
-        private string _address;
-        private string _phone;
-        private string _email;
+        
         private string _ean;
 
         public PublicCustomer(string firstName, string lastName, string adress, string phone, string email, string ean)
             : base(firstName, lastName, adress, phone, email)
         {
-            _firstName = firstName;
-            _lastName = lastName;
-            _address = adress;
-            _phone = phone;
-            _email = email;
+            
             _ean = ean;
-        }
-
-
-
-        public string FirstName
-        {
-            get { return _firstName; }
-        }
-
-        public string LastName
-        {
-            get { return _lastName; }
-        }
-
-        public string Address
-        {
-            get { return _address; }
-        }
-
-        public string Phone
-        {
-            get { return _phone; }
-        }
-
-        public string Email
-        {
-            get { return _email; }
         }
 
         public string EAN
