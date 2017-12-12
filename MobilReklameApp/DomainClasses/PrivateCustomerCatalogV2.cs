6622396d7c7cde4d9a318bffa455e00476085d01
TickleMeeHomo <32668823+TickleMeeHomo@users.noreply.github.com>
2017-12-12 12:42:11 +0100

    CustomerCatalogsV2

--- MobilReklameApp/DomainClasses/PrivateCustomerCatalogV2.cs
+++ MobilReklameApp/DomainClasses/PrivateCustomerCatalogV2.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MobilReklameApp.BaseClasses;
+using MobilReklameApp.SubClasses;
 
 namespace MobilReklameApp.DomainClasses
 {
-    class PrivateCustomerCatalogV2
+    class PrivateCustomerCatalog : CatalogBase<PrivateCustomer>
     {
+        private Dictionary<string, PrivateCustomer> _customers;
+        private static PrivateCustomerCatalog _singletonInstance;
+
+        public PrivateCustomerCatalog()
+        {
+
+            _customers = new Dictionary<string, PrivateCustomer>();
+            _customers.Add("Lars", new PrivateCustomer("Lars", "Larsen", "LarsenVej", "Larsen phone", "LarsenMail"));
+            _customers.Add("Lars", new PrivateCustomer("Lars", "Larsen", "LarsenVej", "Larsen phone", "LarsenMail"));
+            _customers.Add("Lars", new PrivateCustomer("Lars", "Larsen", "LarsenVej", "Larsen phone", "LarsenMail"));
+
+
+        }
+        public List<PrivateCustomer> listAll
+        {
+            get { return _customers.Values.ToList(); }
+        }
+
+        public void Add(PrivateCustomer customer)
+        {
+            _customers.Add(customer.ID, customer);
+        }
+
+        //The following insures that this catalog becomes a singleton
+        //along with the "_singletonInstance" instance field at the top
+        public static PrivateCustomerCatalog SingletonInstance
+        {
+            get
+            {
+                if (_singletonInstance != null) return _singletonInstance;
+                _singletonInstance = new PrivateCustomerCatalog();
+                return _singletonInstance;
+            }
+        }
     }
 }
--- MobilReklameApp/DomainClasses/PublicCustomerCatalogV2.cs
+++ MobilReklameApp/DomainClasses/PublicCustomerCatalogV2.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MobilReklameApp.BaseClasses;
+using MobilReklameApp.SubClasses;
 
 namespace MobilReklameApp.DomainClasses
 {
-    class PublicCustomerCatalogV2
+    class PublicCustomerCatalog : CatalogBase<PublicCustomer>
     {
+        private Dictionary<string, PublicCustomer> _customers;
+        private static PublicCustomerCatalog _singletonInstance;
+
+        public PublicCustomerCatalog()
+        {
+            _customers = new Dictionary<string, PublicCustomer>();
+            _customers.Add("Lars", new PublicCustomer("Lars", "Larsen", "LarsenVej", "Larsen mobil", "LarsenMail", "EAN"));
+            _customers.Add("Lars", new PublicCustomer("Lars", "Larsen", "LarsenVej", "Larsen mobil", "LarsenMail", "EAN"));
+            _customers.Add("Lars", new PublicCustomer("Lars", "Larsen", "LarsenVej", "Larsen mobil", "LarsenMail", "EAN"));
+
+        }
+
+        public List<PublicCustomer> listAll
+        {
+            get { return _customers.Values.ToList(); }
+        }
+
+
+        public void Add(PublicCustomer customer)
+        {
+            _customers.Add(customer.ID, customer);
+        }
+
+        //The following insures that this catalog becomes a singleton
+        //along with the "_singletonInstance" instance field at the top
+        public static PublicCustomerCatalog SingletonInstance
+        {
+            get
+            {
+                if (_singletonInstance != null) return _singletonInstance;
+                _singletonInstance = new PublicCustomerCatalog();
+                return _singletonInstance;
+            }
+        }
+
     }
 }
