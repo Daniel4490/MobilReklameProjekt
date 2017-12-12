ba6e89f8571c685340f2488cc92b73cf1b5309fc
TickleMeeHomo <32668823+TickleMeeHomo@users.noreply.github.com>
2017-12-12 12:27:01 +0100

    CustomerCatalog added

---
+++ MobilReklameApp/DomainClasses/CustomerCatalog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobilReklameApp.BaseClasses;
+using MobilReklameApp.SubClasses;
+
+namespace MobilReklameApp.DomainClasses
+{
+    class CustomerCatalog : CatalogBase<PrivateCustomer>
+    {
+        private Dictionary<string, PrivateCustomer> _customers;
+        private static CustomerCatalog _singletonInstance;
+
+        public CustomerCatalog()
+        {
+
+        }
+        public List<PrivateCustomer> listAll
+        {
+            get { return _customers.Values.ToList(); }
+        }
+
+        public void Add(PrivateCustomer customer)
+        {
+            _customers.Add(customer.ID, customer);
+        }
+
+        //The following insures that this catalog becomes a singleton
+        //along with the "_singletonInstance" instance field at the top
+        public static CustomerCatalog SingletonInstance
+        {
+            get
+            {
+                if (_singletonInstance != null) return _singletonInstance;
+                _singletonInstance = new CustomerCatalog();
+                return _singletonInstance;
+            }
+        }
+    }
+}
