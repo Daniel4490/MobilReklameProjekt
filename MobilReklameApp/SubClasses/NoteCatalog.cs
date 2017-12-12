8dce4d60927aecb07c787112cb16a65ab7049c1e
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2017-12-12 12:05:20 +0100

    Note xaml edit

--- MobilReklameApp/SubClasses/NoteCatalog.cs
+++ MobilReklameApp/SubClasses/NoteCatalog.cs
@@ -22,6 +22,10 @@ namespace MobilReklameApp.SubClasses
             _createNoteCommand = new CreateNoteCommand(_note,this);
 
             _notes = new ObservableCollection<string>();
+            _notes.Add("a note a super long note");
+            _notes.Add("a note");
+            _notes.Add("a note");
+            _notes.Add("a note");
             _notes.Add("a note");
         }
 
