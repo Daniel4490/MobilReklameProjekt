26985c6e721e777100eb8f37db2c5c3969fef184
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2017-12-12 10:45:56 +0100

    It is now possible to add a note to the notification view in MainPage. However the mainpage gui is not updated when a new note is saved while using the gui

---
+++ MobilReklameApp/SubClasses/CreateNoteCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MobilReklameApp.SubClasses
+{
+    class CreateNoteCommand : ICommand
+    {
+        private Note _note;
+        private NoteCatalog _catalog;
+
+        public CreateNoteCommand(Note note, NoteCatalog catalog)
+        {
+            _note = note;
+            _catalog = catalog;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _note != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            _catalog.AddNote();
+
+            _note = new Note();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ MobilReklameApp/SubClasses/Note.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilReklameApp.SubClasses
+{
+    public class Note : INotifyPropertyChanged
+    {
+        private string _note;
+
+        public Note()
+        {
+            _note = "Make a new note by typing here";
+        }
+
+        public string GetNote
+        {
+            get { return _note; }
+            set
+            {
+                _note = value;
+                OnPropertyChanged();
+            }
+        }
+      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
