943413cfd368947cef3ad1d4af7fe2fc51dc4956
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2017-12-14 15:27:12 +0100

    Added missing classes

--- MobilReklameApp/ViewModels/CreateOrderViewModel.cs
+++ MobilReklameApp/ViewModels/CreateOrderViewModel.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace MobilReklameApp.DomainClasses
+namespace MobilReklameApp.ViewModels
 {
     class CreateOrderViewModel : INotifyPropertyChanged
     {
-
-
+       
         public enum CustomerChosen
         {
             Privat,
@@ -27,13 +26,58 @@ namespace MobilReklameApp.DomainClasses
             set
             {
                 _selectedCustomer = value;
+                SetTextEnabled(value);
+                OnPropertyChanged();
+            }
+        }
+        //This makes the combobox able to grey stuff out
+        private void SetTextEnabled(CustomerChosen value)
+        {
+            if (value== CustomerChosen.Privat)
+            {
+                EANenabled = false;
+                CVRenabled = false;
+            }
+            else if (value== CustomerChosen.Erhverv)
+            {
+                EANenabled = false;
+                CVRenabled = true;
+            }
+            else if (value== CustomerChosen.Offentlig)
+            {
+                EANenabled = true;
+                CVRenabled = false;
+            }
+            
+            
+        }
+
+        
+        public bool EANenabled
+        {
+            get { return _eaNenabled; }
+            set
+            {
+                _eaNenabled = value;
                 OnPropertyChanged();
             }
         }
 
-        private ObservableCollection<CustomerChosen> _customerChosens;
-        public CreateOrderViewModel()
+        public bool CVRenabled
         {
+            get { return _cvRenabled; }
+            set
+            {
+                _cvRenabled = value;
+                OnPropertyChanged();
+            }
+        }
+        //This makes the combobox able to grey stuff out
+
+        private ObservableCollection<CustomerChosen> _customerChosens;
+        public CreateOrderViewModel(string mål, string antal, string lam, string medie, string aluSand3Mm, string pvc10Mm, string leveringsAntal, string dtpKr, string opTil10, string fragtKr, string prisKr)
+        {
+            
             //This is to make the ComboBox Work
             _customerChosens = new ObservableCollection<CustomerChosen>();
 
@@ -42,23 +86,9 @@ namespace MobilReklameApp.DomainClasses
             _customerChosens.Add(CustomerChosen.Offentlig);
             //This is to make the ComboBox Work
 
-            
-            Dictionary<CustomerChosen, bool> offentligCustomerState = new Dictionary<CustomerChosen, bool>();
-            offentligCustomerState.Add(CustomerChosen.Offentlig, true);
-            offentligCustomerState.Add(CustomerChosen.Offentlig, false);
 
-            Dictionary<CustomerChosen, bool> erhvervCustomerState = new Dictionary<CustomerChosen, bool>();
-            erhvervCustomerState.Add(CustomerChosen.Erhverv, false);
-            erhvervCustomerState.Add(CustomerChosen.Erhverv, true);
-
-            Dictionary<CustomerChosen, bool> privatCustomerState = new Dictionary<CustomerChosen, bool>();
-            privatCustomerState.Add(CustomerChosen.Privat, false);
-            privatCustomerState.Add(CustomerChosen.Privat, false);
-
-            _allCustomerStates.Add(CustomerChosen.Offentlig, offentligCustomerState);
-            _allCustomerStates.Add(CustomerChosen.Erhverv, erhvervCustomerState);
-            _allCustomerStates.Add(CustomerChosen.Privat, privatCustomerState);
-
+            _eaNenabled = true;
+            _cvRenabled = false;
 
         }
 
@@ -78,13 +108,10 @@ namespace MobilReklameApp.DomainClasses
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-
-
-
-
-
         private CustomerChosen _customerState;
         private Dictionary<CustomerChosen, Dictionary<CustomerChosen, bool>> _allCustomerStates;
+        private bool _eaNenabled;
+        private bool _cvRenabled;
 
         public Dictionary<CustomerChosen, bool> CustomerStates
         {
